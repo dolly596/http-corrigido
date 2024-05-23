b86b8b6df489cf4dc69b34295c1b153fc5ac98c3
dolly596 <83473173+dolly596@users.noreply.github.com>
2024-05-23 11:26:03 -0300

    Http corrigido

---
+++ android/app/src/main/kotlin/com/example/verbos_http_correcao/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verbos_http_correcao
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+
+import 'package:verbos_http_correcao/Principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
+
